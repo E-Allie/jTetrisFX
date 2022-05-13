a724c88db0d3a27931b90588708d809bd5d34f29
ELambertson24 <89552226+ELambertson24@users.noreply.github.com>
2022-05-13 13:43:06 -0400

    Update Main.java
    Added title screen elements: Buttons, Tetris Image, and scaling. Changed the draw function to take in a specific gridpane. Added a score counter to the top of the game window

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -15,6 +16,16 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.util.Random;
 
@@ -28,31 +39,103 @@ public class Main extends Application {
     private FlowPane flowpane;
     private Scene scene;
     private Stage stage;
-
+    private Label pointsLabel;
 
     @Override
     public void start(Stage primaryStage) {
 
         //Create new flowplane;
         flowpane = new FlowPane();
+ 
         //TODO: Ideally more will be displayed than just the grid
 
         //Create a Grid view
         grid = new GridPane();
+        
+        pointsLabel = new Label();
+
         stage = primaryStage;
 
-
-
-        startGameLoop();
-
+        gameMenu();
     }
 
+    public void gameMenu()
+    {
+        GridPane titleGrid = new GridPane();
+        BorderPane bPane = new BorderPane(titleGrid);
+
+        scene = new Scene(bPane);
+
+        Button startGame = new Button("START GAME");
+        Button exitGame = new Button("EXIT GAME");
+        Button loadGame = new Button("LOAD GAME");
+
+        HBox inputs = new HBox(exitGame, startGame, loadGame);
+        inputs.setAlignment(Pos.TOP_CENTER);
+        inputs.setSpacing(5.0);
+
+        Image tetrisImage = new Image("https://i.imgur.com/3QzH1Kh.png");
+        ImageView tetrisView = new ImageView(tetrisImage);
+        tetrisView.setFitWidth(370);
+        tetrisBoard = new Board();
+    
+        VBox topOfMenu = new VBox(tetrisView, inputs);
+        topOfMenu.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+
+        bPane.setTop(topOfMenu);
+        //A very specific pixel height just to make the menu and the game the same height
+        stage.setHeight(706);
+
+        draw(titleGrid);
+
+        EventHandler<MouseEvent> gameStartHandler = new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent e)
+            {
+                System.out.println("Game start");
+                startGameLoop();
+            }
+        };
+
+        EventHandler<MouseEvent> gameLoadHandler = new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent e)
+            {
+                System.out.println("Game loaded");
+                //The game is loaded
+            }
+        };
+
+        EventHandler<MouseEvent> gameEndHandler = new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent e)
+            {
+                System.out.println("Game exit");
+                Platform.exit();
+            }
+        };
+
+        startGame.addEventFilter(MouseEvent.MOUSE_CLICKED, gameStartHandler);
+        loadGame.addEventFilter(MouseEvent.MOUSE_CLICKED, gameLoadHandler);
+        exitGame.addEventFilter(MouseEvent.MOUSE_CLICKED, gameEndHandler);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public void startGameLoop() {
+
         tetrisBoard = new Board();
+        BorderPane bPane = new BorderPane(grid);
+        scene = new Scene(bPane);  //scene will have dimensions equal to the grid
 
-        scene = new Scene(grid);  //scene will have dimensions equal to the grid
-
+        HBox pointsBox = new HBox(pointsLabel);
+        pointsBox.setAlignment(Pos.TOP_CENTER);
+        bPane.setTop(pointsBox);
+        
         /**
          * Here we handle key events
          */
@@ -69,7 +152,8 @@ public class Main extends Application {
                         if (event.getCode() == KeyCode.A) {
                             tetrisBoard = tetrisBoard.pieceCounterOrClockwise(true);
                             System.out.println("APRESSED");
-                            draw();
+                            pointsLabel.setText("Points: " + Integer.toString(tetrisBoard.getPoints()));
+                            draw(grid);
                         }
 
                         /**
@@ -78,7 +162,7 @@ public class Main extends Application {
                         if (event.getCode() == KeyCode.D) {
                             tetrisBoard = tetrisBoard.pieceCounterOrClockwise(false);
                             System.out.println("DPRESSED");
-                            draw();
+                            draw(grid);
                         }
 
                         /**
@@ -87,19 +171,19 @@ public class Main extends Application {
                         if (event.getCode() == KeyCode.S || event.getCode() == KeyCode.DOWN) {
                             tetrisBoard = tetrisBoard.pieceLDR(Board.Direction.Down);
                             System.out.println("DOWNPRESSED");
-                            draw();
+                            draw(grid);
                         }
 
                         if (event.getCode() == KeyCode.LEFT) {
                             tetrisBoard = tetrisBoard.pieceLDR(Board.Direction.Left);
                             System.out.println("LeftPRESSED");
-                            draw();
+                            draw(grid);
                         }
 
                         if (event.getCode() == KeyCode.RIGHT) {
                             tetrisBoard = tetrisBoard.pieceLDR(Board.Direction.Right);
                             System.out.println("RightPRESSED");
-                            draw();
+                            draw(grid);
                         }
                     }
                 }
@@ -116,7 +200,7 @@ public class Main extends Application {
                 System.out.println("HANDLE GAME OVER");
 
             }
-            draw();
+            draw(grid);
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
@@ -130,7 +214,7 @@ public class Main extends Application {
      * Destructs the old elements of the grid from last draw.
      * Then adds the "new" board.
      */
-    public void draw() {
+    public void draw(GridPane grid) {
 
         /**
          * This line is incredibly necessary. Without it, every previous grid "exists", stacked under the "top" grid.
@@ -161,6 +245,7 @@ public class Main extends Application {
             }
         }
 
+        pointsLabel.setText("Score: " + Integer.toString(tetrisBoard.getPoints()));
 
         stage.setScene(scene);
         stage.show();
